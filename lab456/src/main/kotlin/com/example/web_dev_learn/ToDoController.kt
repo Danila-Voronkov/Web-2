902df430fd0e553f8018058a30b5af73a8d40124
Danila-Voronkov <94179503+Danila-Voronkov@users.noreply.github.com>
2024-06-20 10:15:32 +0700

    Add files via upload

---
+++ lab456/src/main/kotlin/com/example/web_dev_learn/ToDoController.kt
@@ -0,0 +1,91 @@
+package com.example.web_dev_learn
+
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
+@RestController
+@RequestMapping("/api")
+class ToDoController(
+    private val todoRepo: ToDoRepository
+) {
+    init {
+        todoRepo.save(
+            TodoItem(
+                title = "test title",
+                description = "test description",
+                status = Status.TODO,
+                date = Date(),
+                subtodos = emptyList()
+            )
+        )
+    }
+
+    @GetMapping("/todo")
+    fun getTodos(): List<TodoItem> {
+        return todoRepo.findAll()
+    }
+
+    @GetMapping("/todo/{id}")
+    fun getTodo(@PathVariable id: Long): TodoItem {
+        return todoRepo.findById(id).get()
+    }
+
+    @PostMapping("/todo")
+    fun createTodo(
+        @RequestBody httpEntity: TodoItemRequestBody
+    ): TodoItem {
+        val subtodos = httpEntity.subtodosJson?.map {
+            TodoItem(
+                title = it.title.orEmpty(),
+                description = it.desc,
+                status = Status.TODO,
+                date = Date(),
+                subtodos = emptyList()
+            )
+        }
+        return todoRepo.save(
+            TodoItem(
+                title = httpEntity.title.orEmpty(),
+                description = httpEntity.desc,
+                status = Status.TODO,
+                date = Date(),
+                subtodos = subtodos.orEmpty()
+            )
+        )
+    }
+
+    @PutMapping("/todo/{id}")
+    fun editTodo(
+        @RequestBody httpEntity: TodoItemRequestBody,
+        @PathVariable id: Long
+    ): TodoItem {
+        val subtodos = httpEntity.subtodosJson?.map {
+            TodoItem(
+                title = it.title.orEmpty(),
+                description = it.desc,
+                status = Status.TODO,
+                date = Date(),
+                subtodos = emptyList()
+            )
+        }
+
+        val todo = todoRepo.findById(id).get()
+        val newTodo = todo.copy(
+            title = httpEntity.title.orEmpty(),
+            description = httpEntity.desc,
+            status = Status.TODO,
+            date = Date(),
+            subtodos = subtodos.orEmpty()
+        )
+        todoRepo.deleteById(id)
+
+        return todoRepo.save(newTodo)
+    }
+
+    @DeleteMapping("todo/{id}")
+    fun deleteTodo(
+        @PathVariable id: Long
+    ) {
+        todoRepo.deleteById(id)
+    }
+}
